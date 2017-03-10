4652630b8684ef383cce6932886b93d85f4926cd
sarindy <sarindyo@yahoo.com>
2017-03-10 21:51:21 +0700

    001

--- src/main/java/org/sarindy/JpaRelationShipApplication.java
+++ src/main/java/org/sarindy/JpaRelationShipApplication.java
@@ -1,12 +1,32 @@
 package org.sarindy;
 
+
+
+import org.sarindy.OneToOne.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaRelationShipApplication {
+public class JpaRelationShipApplication implements CommandLineRunner {
+	
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(JpaRelationShipApplication.class);
+	
+	@SuppressWarnings("unused")
+	@Autowired
+	private BookRepository bookRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaRelationShipApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ src/main/java/org/sarindy/OneToOne/Book.java
@@ -0,0 +1,66 @@
+package org.sarindy.OneToOne;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+	private int id;
+	private String name;
+	private BookDetail bookDetail;
+
+	public Book() {
+
+	}
+
+	public Book(String name) {
+		this.name = name;
+	}
+
+	public Book(String name, BookDetail bookDetail) {
+		this.name = name;
+		this.bookDetail = bookDetail;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "book_detail_id")
+	public BookDetail getBookDetail() {
+		return bookDetail;
+	}
+
+	public void setBookDetail(BookDetail bookDetail) {
+		this.bookDetail = bookDetail;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Book[id=%d, name='%s', number of pages='%d']", id, name, bookDetail.getNumberOfPages());
+	}
+
+}
