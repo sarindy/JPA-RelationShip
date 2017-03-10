7c8a761936fcc5a7b1dfe604878251833ed8b073
sarindy <sarindyo@yahoo.com>
2017-03-10 21:31:25 +0700

    First Commit

---
+++ src/main/java/org/sarindy/JpaRelationShipApplication.java
@@ -0,0 +1,12 @@
+package org.sarindy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationShipApplication.class, args);
+	}
+}
